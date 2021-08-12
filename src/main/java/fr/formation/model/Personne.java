64c8eb744cf079fa305999095320c8a536d537c0
Pierre SCHICKELE <pierre.schickele@live.fr>
2021-08-12 10:36:35 +0200

    mapping Personne java

--- src/main/java/fr/formation/model/Personne.java
+++ src/main/java/fr/formation/model/Personne.java
@@ -3,20 +3,106 @@ package fr.formation.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "personne")
 public class Personne {
-	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "PER_ID")
 	private int id;
+	
+	@Column(name = "PER_NOM", nullable = false)
 	private String nom;
+	
+	@Column(name = "PER_PRENOM", nullable = false)
 	private String prenom;
+	
+	@Column(name = "PER_DATE_NAISSANCE", nullable = false)
 	private LocalDate dateNaissance;
-	private Habitation habitation;
-	private Metier metier;
+	
+	@Column(name = "PER_ARGENT")
 	private BigDecimal argent;
 	
+	@ManyToOne
+	@JoinColumn(name = "PER_HABITATION_ID")
+	private Habitation habitation;
+	
+	@ManyToOne
+	@JoinColumn(name = "PER_METIER_ID")
+	private Metier metier;
+
+	public Personne() {
+	}
+
+	public Personne(String nom, String prenom, LocalDate dateNaissance, BigDecimal argent) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.argent = argent;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public LocalDate getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(LocalDate dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	public BigDecimal getArgent() {
+		return argent;
+	}
+
+	public void setArgent(BigDecimal argent) {
+		this.argent = argent;
+	}
+
+	public Habitation getHabitation() {
+		return habitation;
+	}
+
+	public void setHabitation(Habitation habitation) {
+		this.habitation = habitation;
+	}
+
+	public Metier getMetier() {
+		return metier;
+	}
+
+	public void setMetier(Metier metier) {
+		this.metier = metier;
+	}
 }
