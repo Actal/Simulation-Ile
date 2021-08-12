5a215d129eb6e64b734bd7d991a8ab84624e0e3d
Actal <aimeric.bruno@gmail.com>
2021-08-12 11:00:22 +0200

    getter setter constructeur mapping habitation

--- src/main/java/fr/formation/model/Habitation.java
+++ src/main/java/fr/formation/model/Habitation.java
@@ -1,9 +1,23 @@
 package fr.formation.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-public class Habitation {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "habitation")
+public class Habitation extends Batiment {
+
+	@Column(name = "HAB_NB_PLACE", nullable = false)
 	private int nbPlace;
+	
+	@Column(name = "HAB_LOYER", nullable = false)
 	private BigDecimal loyer;
+	
+	@OneToMany(mappedBy = "habitation")
+	private List<Personne> habitants;
 }
