6d0258b620df76c10f78c5da180eb683a4bf1eef
Softpurr <chakatsoftpurr@hotmail.com>
2021-08-13 11:57:01 +0200

    service

--- src/main/java/fr/formation/model/Service.java
+++ src/main/java/fr/formation/model/Service.java
@@ -4,15 +4,19 @@ import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table (name = "service")
 public class Service extends Workplace{
 
+	@Column(name = "SER_ID")
 	private BigDecimal prixEntree;
 	
+	@OneToMany(mappedBy ="service")
 	private List<Personne> clients;
 
 	
