f8e8b6a323d54ecd728035ef4a1469bf26140c09
Actal <aimeric.bruno@gmail.com>
2021-08-13 15:50:55 +0200

    maj changement constructeur

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -12,6 +12,7 @@ import fr.formation.dao.IHabitationDao;
 import fr.formation.dao.IMetierDao;
 import fr.formation.dao.IPersonneDao;
 import fr.formation.dao.IPosteDao;
+import fr.formation.dao.IProprietaireDao;
 import fr.formation.dao.IWorkplaceDao;
 import fr.formation.dao.jpa.AdresseDaoJpa;
 import fr.formation.dao.jpa.BiomeDaoJpa;
@@ -20,44 +21,49 @@ import fr.formation.dao.jpa.HabitationDaoJpa;
 import fr.formation.dao.jpa.MetierDaoJpa;
 import fr.formation.dao.jpa.PersonneDaoJpa;
 import fr.formation.dao.jpa.PosteDaoJpa;
+import fr.formation.dao.jpa.ProprietaireDaoJpa;
 import fr.formation.dao.jpa.WorkplaceDaoJpa;
 import fr.formation.model.*;
 
 public class Application {
 
 	public static void main(String[] args) {
-		IAdresseDao daoA = new AdresseDaoJpa();
-		IBiomeDao daoB = new BiomeDaoJpa();
-		ICoordonneesDao daoC = new CoordonneesDaoJpa();
-		IHabitationDao daoH = new HabitationDaoJpa();
-		IMetierDao daoM = new MetierDaoJpa();
-		IPersonneDao daoP = new PersonneDaoJpa();
+		IAdresseDao daoAdresse = new AdresseDaoJpa();
+		IBiomeDao daoBiome = new BiomeDaoJpa();
+		ICoordonneesDao daoCoordonnees = new CoordonneesDaoJpa();
+		IHabitationDao daoHabitation = new HabitationDaoJpa();
+		IMetierDao daoMetier = new MetierDaoJpa();
+		IProprietaireDao daoProprietaire = new ProprietaireDaoJpa();
+		IPersonneDao daoCitoyen = new PersonneDaoJpa();
 		IPosteDao daoPoste = new PosteDaoJpa();
-		IWorkplaceDao daoW = new WorkplaceDaoJpa();
+		IWorkplaceDao daoWorkplace = new WorkplaceDaoJpa();
 				
-		Personne p1 = new Personne("Bruno", "Aimeric", LocalDate.of(2010,Month.FEBRUARY,10), new BigDecimal(100));
-		Personne p2 = new Personne("Luck", "Romain", LocalDate.of(2008,Month.FEBRUARY,15), new BigDecimal(10000));
-		Personne p3 = new Personne("Schickele", "Pierre", LocalDate.of(2010,Month.FEBRUARY,10), new BigDecimal(646));
+		Citoyen citoyen1 = new Citoyen("Bruno", "Aimeric", LocalDate.of(2010,Month.FEBRUARY,10), new BigDecimal(100));
+		Citoyen citoyen2 = new Citoyen("Luck", "Romain", LocalDate.of(2008,Month.FEBRUARY,15), new BigDecimal(10000));
+		Citoyen citoyen3 = new Citoyen("Schickele", "Pierre", LocalDate.of(2010,Month.FEBRUARY,10), new BigDecimal(646));
 		
-		Coordonnees c1 = new Coordonnees(10,10);
-		Coordonnees c2 = new Coordonnees(45,456);
+		Proprietaire proprio = new Proprietaire("Truc", "Machin", LocalDate.of(1985,Month.FEBRUARY,10), new BigDecimal(1000000));
 		
-		Adresse a1 = new Adresse("Paix", 45);
-		a1.setCoordonnees(c1);
-		Adresse a2 = new Adresse("Paix", 46);
-		a2.setCoordonnees(c2);
+		Coordonnees coor1 = new Coordonnees(10,10);
+		Coordonnees coor2 = new Coordonnees(45,456);
+		
+		Adresse adr1 = new Adresse("rue de la Paix", 45);
+		adr1.setCoordonnees(coor1);
+		Adresse adr2 = new Adresse("avenue des champs elysees", 46);
+		adr2.setCoordonnees(coor2);
 		
 		Biome b = new Biome("plaine", new BigDecimal(1666));
-		b.setCoordonnees(c2); 
+		b.setCoordonnees(coor2); 
 		
-		Habitation h1 = new Habitation(a1, new BigDecimal(45), b, 4, new BigDecimal(50));
-		Workplace w1 = new Workplace(a2, new BigDecimal(45), b, LocalTime.of(9, 0), LocalTime.of(17, 0));
+		Habitation h1 = new Habitation(new BigDecimal(45), "Maison", new BigDecimal(1000), new BigDecimal(100), 4, adr1, b, proprio, new BigDecimal(50));
+		
+		citoyen1.setHabitation(h1);
+		citoyen2.setHabitation(h1);
+		citoyen3.setHabitation(h1);
+		
+		Workplace w1 = new Workplace(new BigDecimal(45), "Maison", new BigDecimal(1000), new BigDecimal(100), 4, adr1, b, proprio, LocalTime.of(9, 0), LocalTime.of(18, 0));
 		
 		Metier m1 = new Metier("Informaticien");
-		
-		p1.setHabitation(h1);
-		p2.setHabitation(h1);
-		p3.setHabitation(h1);
 		
 		Poste poste1 = new Poste(new BigDecimal(100));
 		Poste poste2 = new Poste(new BigDecimal(150));
@@ -67,34 +73,36 @@ public class Application {
 		poste2.setMetier(m1);
 		poste3.setMetier(m1);
 		
-		p1.setPoste(poste1);
-		p2.setPoste(poste2);
-		p3.setPoste(poste3);
+		citoyen1.setPoste(poste1);
+		citoyen2.setPoste(poste2);
+		citoyen3.setPoste(poste3);
 		
 		poste1.setWorkplace(w1);
 		poste2.setWorkplace(w1);
 		poste3.setWorkplace(w1);
 		
-		daoC.save(c1);
-		daoC.save(c2);
+		daoCoordonnees.save(coor1);
+		daoCoordonnees.save(coor2);
 		
-		daoA.save(a1);
-		daoA.save(a2);
+		daoAdresse.save(adr1);
+		daoAdresse.save(adr2);
 		
-		daoB.save(b);
+		daoBiome.save(b);
 		
-		daoH.save(h1);
-		daoW.save(w1);
+		daoHabitation.save(h1);
+		daoWorkplace.save(w1);
 		
-		daoM.save(m1);
+		daoMetier.save(m1);
 
 		daoPoste.save(poste1);
 		daoPoste.save(poste2);
 		daoPoste.save(poste3);
 		
-		daoP.save(p1);
-		daoP.save(p2);
-		daoP.save(p3);
+		daoCitoyen.save(citoyen1);
+		daoCitoyen.save(citoyen2);
+		daoCitoyen.save(citoyen3);
+		
+		daoProprietaire.save(proprio);
 	}
 
 }
