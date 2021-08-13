b793899baf454754440d9bb36465e6f6a3822a37
Pierre SCHICKELE <pierre.schickele@live.fr>
2021-08-13 11:46:38 +0200

    modif classe Habitation

--- src/main/java/fr/formation/model/Habitation.java
+++ src/main/java/fr/formation/model/Habitation.java
@@ -13,20 +13,18 @@ import javax.persistence.Table;
 @Table(name = "habitation")
 @PrimaryKeyJoinColumn(name="HAB_ID", referencedColumnName="BAT_ID")
 public class Habitation extends Batiment {
-
-	@Column(name = "HAB_NB_PLACE", nullable = false)
-	private int nbPlace;
-	
 	@Column(name = "HAB_LOYER", nullable = false)
 	private BigDecimal loyer;
 	
 	@OneToMany(mappedBy = "habitation")
-	private List<Personne> habitants;
+	private List<Citoyen> habitants;
 
-	public void ajouterHabitant(Personne nvHabitant) {
+	public void ajouterHabitant(Citoyen nvHabitant) {
 		int nbHab = habitants.size();
-		if(nbHab < nbPlace)
+		if(nbHab < nbPlace) {
 			habitants.add(nvHabitant);
+			nvHabitant.setHabitation(this);
+		}
 		else
 			System.out.println("Habitation pleine ! Pas d'ajout d'habitant");
 	}
@@ -47,20 +45,19 @@ public class Habitation extends Batiment {
 		this.loyer = loyer;
 	}
 
-	public List<Personne> getHabitants() {
+	public List<Citoyen> getHabitants() {
 		return habitants;
 	}
 
-	public void setHabitants(List<Personne> habitants) {
+	public void setHabitants(List<Citoyen> habitants) {
 		this.habitants = habitants;
 	}
 
 	public Habitation() {
 	}
 
-	public Habitation(Adresse adresse, BigDecimal superficie, Biome biome, int nbPlace, BigDecimal loyer) {
-		super(adresse, superficie, biome);
-		this.nbPlace = nbPlace;
+	public Habitation(String nom, BigDecimal superficie, BigDecimal prix, BigDecimal coutEntretien, int nbPlace, BigDecimal loyer) {
+		super(nom, superficie, prix, coutEntretien, nbPlace);
 		this.loyer = loyer;
 	}
 
