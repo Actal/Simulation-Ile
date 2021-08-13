83517bacd21a93e0b3a37e57320bb8b8a9239ee5
Actal <aimeric.bruno@gmail.com>
2021-08-13 11:51:49 +0200

    Ajout citoyen

---
+++ src/main/java/fr/formation/model/Citoyen.java
@@ -0,0 +1,54 @@
+package fr.formation.model;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+public class Citoyen extends Personne {
+
+	@OneToOne
+	@JoinColumn(name = "PER_POSTE_ID")
+	private Poste poste;
+	
+	@ManyToOne
+	@JoinColumn(name = "PER_HABITATION_ID")
+	private Habitation habitation;
+	
+	public void payerLoyer() {
+		this.setArgent(this.getArgent().subtract(this.getHabitation().getLoyer()));
+	}
+	
+	public void allerTravailler() {
+		this.setCoordonnees(this.getPoste().getWorkplace().getAdresse().getCoordonnees());
+	}
+	
+	public void rentrer() {
+		this.setCoordonnees(this.getHabitation().getAdresse().getCoordonnees());
+	}
+
+	public Boolean isTravailleur() {
+		return this.getPoste() != null;
+	}
+	
+	public Boolean isLocataire() {
+		return this.getHabitation() != null;
+	}
+	
+	public Poste getPoste() {
+		return poste;
+	}
+
+	public void setPoste(Poste poste) {
+		this.poste = poste;
+	}
+
+	public Habitation getHabitation() {
+		return habitation;
+	}
+
+	public void setHabitation(Habitation habitation) {
+		this.habitation = habitation;
+	}
+	
+	
+}
---
+++ src/main/java/fr/formation/model/Sexe.java
@@ -0,0 +1,6 @@
+package fr.formation.model;
+
+public enum Sexe {
+	Homme,
+	Femme;
+}
