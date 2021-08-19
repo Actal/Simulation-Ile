267ce14830b68a70e45f842794bd188e3d4f86ca
Actal <aimeric.bruno@gmail.com>
2021-08-19 10:53:51 +0200

    suppresion import

--- src/main/java/fr/formation/Application.java
+++ src/main/java/fr/formation/Application.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
