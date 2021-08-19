d12513d7faa4eab4fe68b5b6722038ec4f53e803
PierreSchickele <pierre.schickele@live.fr>
2021-08-19 12:27:31 +0200

    ajout de methode chercherTravail()

--- src/main/java/fr/formation/service/CitoyenService.java
+++ src/main/java/fr/formation/service/CitoyenService.java
@@ -9,18 +9,29 @@ import org.springframework.stereotype.Service;
 
 import fr.formation.dao.ICitoyenDao;
 import fr.formation.dao.IHabitationDao;
+import fr.formation.dao.IPosteDao;
+import fr.formation.dao.IWorkplaceDao;
 import fr.formation.model.Citoyen;
 import fr.formation.model.Habitation;
+import fr.formation.model.Metier;
+import fr.formation.model.Poste;
+import fr.formation.model.Workplace;
 
 @Service
 @Transactional
 public class CitoyenService extends PersonneService {
 	@Autowired
 	private ICitoyenDao daoCitoyen;
-	
+
 	@Autowired
 	private IHabitationDao daoHabitation;
-	
+
+	@Autowired
+	private IPosteDao daoPoste;
+
+	@Autowired
+	private IWorkplaceDao daoWorkplace;
+
 	@Autowired
 	private HabitationService habitationService;
 
@@ -50,17 +61,41 @@ public class CitoyenService extends PersonneService {
 		return (citoyen.getHabitation() != null);
 	}
 
-	public void checherLogement(int idCitoyen ) {
+	public void checherLogement(int idCitoyen) {
 		Citoyen citoyen = daoCitoyen.findById(idCitoyen).get();
 		List<Habitation> habitations = daoHabitation.findAll();
-		
-		for( Habitation h : habitations) {
-			if( h.getHabitants().size() < h.getNbPlace()) {
-				if( citoyen.getArgent().compareTo(h.getLoyer()) > 0 ) {
+
+		for (Habitation h : habitations) {
+			if (h.getHabitants().size() < h.getNbPlace()) {
+				if (citoyen.getArgent().compareTo(h.getLoyer()) > 0) {
 					citoyen.setHabitation(h);
 					habitationService.ajouterHabitant(h.getId(), citoyen);
 				}
 			}
 		}
+	}
+
+	public void chercherTravail(int idCitoyen) {
+		Citoyen citoyen = daoCitoyen.findById(idCitoyen).get();
+		List<Workplace> workplaces = daoWorkplace.findAll();
+		boolean aTrouve = false;
+
+		for (Workplace w : workplaces) {
+			for (Poste p : w.getPostes()) {
+				if (p.getCitoyen() == null) {
+					citoyen.setPoste(p);
+					p.setCitoyen(citoyen);
+					daoCitoyen.save(citoyen);
+					daoPoste.save(p);
+					aTrouve = true;
+					break;
+				}
+			}
+			if (aTrouve == true) {
+				daoWorkplace.save(w);
+				break;
+			}
+		}
+
 	}
 }
