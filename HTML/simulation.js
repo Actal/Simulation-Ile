89a44d0fb9e8ba4c14bf38aa203a4c91b33b3183
Softpurr <chakatsoftpurr@hotmail.com>
2021-08-25 12:38:26 +0200

    simulationhtml

--- HTML/simulation.js
+++ HTML/simulation.js
@@ -1,25 +1,62 @@
 
-let table = document.querySelector("#simulation-area table");
+//Stocke les coordonnees avec les informations associees
+let coordonnees = [];
+for (let y=0; y<100; y++){
+    let tmpTab = [];
+    for (let x=0; x<100; x++){
+        tmpTab.push({nom:"null"});
+    }
+    coordonnees.push(tmpTab);
+}
 
+let batiments = [
+    {
+        id: 1,
+        nom: "Usine",
+        x: 5,
+        y: 5
+    },
+    {
+        id: 2,
+        nom: "Ecole",
+        x: 20,
+        y: 37
+    },
+    {
+        id: 3,
+        nom: "Caserne",
+        x: 51,
+        y: 60
+    }
+]
+
+//Cree le tableau pour la simulation
+let table = document.querySelector("#simulation-area table");
 let stringTab = "";
-for (let x=0; x<100; x++){
-    let classLine = x%2;
+for (y in coordonnees){
+    let classLine = y%2;
     stringTab += `<tr class="line-${classLine}">`;
-    for (let y=0; y<100; y++){
-        let classColumn = y%2;
-        stringTab += `<th class="column-${classColumn}" ></th>`;
+    for (x in coordonnees){
+        let classColumn = x%2;
+        stringTab += `<td id="coordonnee-${x}-${y}"></td>`;
     }
     stringTab += "</tr>";
 }
 table.innerHTML = stringTab;
 
-let cases = document.querySelectorAll("th");
-/*for (case of cases){
-    case.addEventListener('hover', () => {
-        document.querySelector("#sidebar").textContent = this.className;
-    });
-}*/
+//Initialise les batiments dans les donnees de la page
+for (b of batiments){
+    document.querySelector(`#coordonnee-${b.x}-${b.y}`).style.backgroundColor = "red";
+    coordonnees[b.x][b.y] = b;
+}
 
-table.addEventListener('hover', () => {
-    document.querySelector("#sidebar").textContent = "test";
+//Affiche information en passant avec la souris
+let tbody = document.querySelector("#simulation-area tbody");
+tbody.addEventListener('mouseover', (event) => {
+    let elem = document.querySelector("#sidebar");
+    let coord = event.target.id;
+    coord = coord.split("-");
+    elem.textContent = coordonnees[parseInt(coord[1])][parseInt(coord[2])].nom;
+    //event.target.style.backgroundColor = "blue";
 });
+
