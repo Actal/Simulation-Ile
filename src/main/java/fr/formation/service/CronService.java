acee9476e0fa1caca8ea216c562c1d8268f1ee62
Softpurr <chakatsoftpurr@hotmail.com>
2021-09-03 16:26:23 +0200

    cron

--- src/main/java/fr/formation/service/CronService.java
+++ src/main/java/fr/formation/service/CronService.java
@@ -1,19 +1,41 @@
 package fr.formation.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import fr.formation.dao.IProprietaireDao;
+import fr.formation.model.Proprietaire;
+
 @Service
 public class CronService {
+	@Autowired
+	private IProprietaireDao daoProprietaire;
+	
+	@Autowired
+	private ProprietaireService proprietaireService;
+	
 	//Toutes les minutes, pour simuler une heure
 	@Scheduled(fixedRate = 1000*60) //Scheduled : il faut activer cette annotation dans la configuration
 	public void heureCron() {
-		System.out.println("Coucou CRON minute");
+		/*
+		 * Action personne
+		 * */
+		System.out.println("Coucou CRON heure");
 	}
 	
 	//Simule un mois, appele des fonctions de fin de mois comme payer loyer
-	@Scheduled(fixedRate = 1000*60*24*30) //Scheduled : il faut activer cette annotation dans la configuration
+	@Scheduled(fixedRate = 1000*60*24*7) //Scheduled : il faut activer cette annotation dans la configuration
 	public void moisCron() {
 		System.out.println("Coucou CRON mois");
+
+		/*List<Proprietaire> proprietaires = daoProprietaire.findAll();
+		for (Proprietaire p: proprietaires){
+			proprietaireService.payerEmployes(p.getId());
+			proprietaireService.percevoirBenefice(p.getId());
+		}*/
+		
 	}
 }
