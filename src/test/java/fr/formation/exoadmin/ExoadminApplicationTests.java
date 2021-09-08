49b5f8808515d9e6e316d9f520ca1d1ba941d806
Softpurr <chakatsoftpurr@hotmail.com>
2021-09-08 14:23:33 +0200

    everything

---
+++ src/test/java/fr/formation/exoadmin/ExoadminApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.exoadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
