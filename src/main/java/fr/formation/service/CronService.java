ebc62c3ccf9e68c42872a4cc9eceb9bb9480e8f0
PierreSchickele <pierre.schickele@live.fr>
2021-09-09 09:52:08 +0200

    modif CronService affichage date heure et collecte donnees bdd en semaine

--- src/main/java/fr/formation/service/CronService.java
+++ src/main/java/fr/formation/service/CronService.java
@@ -2,14 +2,19 @@ package fr.formation.service;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import fr.formation.dao.ICitoyenDao;
+import fr.formation.dao.IPersonneDao;
 import fr.formation.dao.IProprietaireDao;
 import fr.formation.model.Citoyen;
+import fr.formation.model.Personne;
+import fr.formation.model.Proprietaire;
 import fr.formation.model.SimulationEtat;
 
 @Service
@@ -27,14 +32,18 @@ public class CronService {
 	private CitoyenService citoyenService;
 
 	@Autowired
+	private IPersonneDao daoPersonne;
+
+	@Autowired
 	SimulationEtat simulationEtat;
 
 	// Toutes les 6 secondes, pour simuler une heure
 	@Scheduled(fixedRate = 1000 * 6) // Scheduled : il faut activer cette annotation dans la configuration
 	public void heureCron() {
-		System.out.println("Coucou CRON heure");
+		LocalTime time = simulationEtat.getTime().toLocalTime();
+		LocalDate date = simulationEtat.getTime().toLocalDate();
 
-		localTime time = simulationEtat.getTime().toLocalTime();
+		System.out.println("CRON heure... Date : " + date.toString() + " Time : " + time.toString());
 
 		for (Citoyen f : daoCitoyen.findAll()) {
 			citoyenService.faireAction(f.getId(), time);
@@ -46,24 +55,33 @@ public class CronService {
 	// Simule une semaine, appele des fonctions de fin de semaine comme payer loyer
 	@Scheduled(fixedRate = 1000 * 6 * 24 * 7) // Scheduled : il faut activer cette annotation dans la configuration
 	public void semaineCron() {
-		System.out.println("Coucou CRON semaine");
+		LocalDate date = simulationEtat.getTime().toLocalDate();
+
+		System.out.println("CRON semaine... Date : " + date.toString());
 
 		List<Proprietaire> proprietaires = daoProprietaire.findAll(); for
 		(Proprietaire p: proprietaires){
 			proprietaireService.payerEmployes(p.getId());
 			proprietaireService.percevoirBenefice(p.getId());
 		}
-		 
+		
+		BigDecimal argenttotal = new BigDecimal(0);
+		BigDecimal argentproprietaires = new BigDecimal(0);
+		List<Personne> personnes = daoPersonne.findAll();
+		for (Personne p : personnes) {
+			argenttotal = argenttotal.add(p.getArgent());
+			if (p instanceof Proprietaire) {
+				argentproprietaires = argentproprietaires.add(p.getArgent());
+			}
+		}
 
-		LocalDate date = simulationEtat.getTime().toLocalDate();
-
-		// BigDecimal[] desValues = new BigDecimal[3];
-		// desValues[0] = new BigDecimal(40000);
-		// desValues[1] = new BigDecimal(50000);
-		// desValues[2] = new BigDecimal(90000);
+		BigDecimal[] desValues = new BigDecimal[3];
+		desValues[0] = argenttotal.subtract(argentproprietaires);
+		desValues[1] = argentproprietaires;
+		desValues[2] = argenttotal;
 
 		EditCsvService editCsv = new EditCsvService();
-		editCsv.write(uneDate, desValues);
+		editCsv.write(date, desValues);
 
 	}
 }
