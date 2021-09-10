f220403c9269dd29aaf8dfb5c21b572b9eef2147
Actal <aimeric.bruno@gmail.com>
2021-09-10 14:14:43 +0200

    dbug modification batiment

--- src/main/java/fr/formation/service/BatimentService.java
+++ src/main/java/fr/formation/service/BatimentService.java
@@ -19,10 +19,11 @@ public abstract class BatimentService {
 	public boolean estConstructible(Batiment batimentAConstruire) {
 		
 		for( Batiment b : daoBatiment.findAll()) {
-			if( b.getCoordonnees().getX() + b.getLongueur()< batimentAConstruire.getCoordonnees().getX()) continue;
-			if( b.getCoordonnees().getY() + b.getLongueur()< batimentAConstruire.getCoordonnees().getY()) continue;
-			if( b.getCoordonnees().getX() > batimentAConstruire.getCoordonnees().getX() + batimentAConstruire.getLongueur()) continue;
-			if( b.getCoordonnees().getY() > batimentAConstruire.getCoordonnees().getY() + batimentAConstruire.getLongueur()) continue;
+			if (b.getId() == batimentAConstruire.getId()) continue;
+				if( b.getCoordonnees().getX() + b.getLongueur()< batimentAConstruire.getCoordonnees().getX()) continue;
+				if( b.getCoordonnees().getY() + b.getLongueur()< batimentAConstruire.getCoordonnees().getY()) continue;
+				if( b.getCoordonnees().getX() > batimentAConstruire.getCoordonnees().getX() + batimentAConstruire.getLongueur()) continue;
+				if( b.getCoordonnees().getY() > batimentAConstruire.getCoordonnees().getY() + batimentAConstruire.getLongueur()) continue;
 			
 			return false;
 		}
